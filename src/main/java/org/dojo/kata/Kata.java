1ca97d8d955d79ab460f19687c3f826317c5d685
Remy Sanlaville <remy.sanlaville@gmail.com>
2018-03-04 19:05:53 +0100

    Create dojo template for java
    Src:  Java 1.8
    Test: Junit4  + Fest Assert + Zohhak

---
+++ src/main/java/org/dojo/kata/Kata.java
@@ -0,0 +1,5 @@
+package org.dojo.kata;
+
+public class Kata {
+
+}
---
+++ src/test/java/org/dojo/kata/KataTest.java
@@ -0,0 +1,12 @@
+package org.dojo.kata;
+
+import org.fest.assertions.api.Assertions;
+import org.junit.Test;
+
+public class KataTest {
+
+  @Test
+  public void check_environment() {
+    Assertions.assertThat(false).isTrue();
+  }
+}
