16471085a8bca4b6d4452c03e8e87f022af28597
Remy Sanlaville <remy.sanlaville@gmail.com>
2019-09-22 19:09:21 +0200

    Migrate to JUnit5 and AssertJ

--- src/test/java/org/dojo/kata/KataTest.java
+++ src/test/java/org/dojo/kata/KataTest.java
@@ -1,12 +1,27 @@
 package org.dojo.kata;
 
-import org.fest.assertions.api.Assertions;
-import org.junit.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+@DisplayName("JUnit5 Starter")
 public class KataTest {
 
-  @Test
-  public void check_environment() {
-    Assertions.assertThat(false).isTrue();
-  }
+    @Test
+    @DisplayName("Check my environment in order to be able to start...")
+    public void check_environment() {
+        Assertions.assertThat(false).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "false, true",
+        "true, false"
+    })
+    @DisplayName("Check my environment with a parameterized test")
+    public void check_environment(boolean actuel, boolean expected){
+        Assertions.assertThat(actuel).isEqualTo(expected);
+    }
 }
